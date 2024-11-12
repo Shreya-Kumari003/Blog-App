907152ae5fa094eab896c745c93d6f7a02ad4ed5
Raj Shekhar Sinha <raj18110sinha@gmail.com>
2024-11-13 00:10:56 +0530

    integrating firebase

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
+import { store, persistor } from './redux/store.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      {/* <ThemeProvider> */}
+        <App />
+      {/* </ThemeProvider> */}
+    </Provider>
+  </PersistGate>
 )
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,7 @@
+import React from 'react'
 
-function Dashboard() {
+export default function Dashboard() {
   return (
     <div>Dashboard</div>
   )
 }
-
-export default Dashboard
